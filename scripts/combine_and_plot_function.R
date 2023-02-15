8869194cf4446c9a99ad09c595a46c63f0b7dc24
Justin Isip <justinisip@outlook.com>
2023-02-15 17:53:04 +0000

    Commit functions

---
+++ scripts/combine_and_plot_function.R
@@ -0,0 +1,61 @@
+
+combine_and_plot <- function(effect1, effect2, relevel) {
+  
+  combined <- rbind(effect1, effect2)
+  
+  # Add in a column of Order to make sure there is a grouping variable
+  comb_plot_limits <- c("Primary", "YSV", "ISV", "MSV", "Plantation", "Pasture", "Cropland", "Urban") 
+  
+  # If I don't supply relevel as an argument then do nothing
+  if(missing(relevel)) {
+    NULL
+  } 
+  # If I do, then relevel the plot based on the reference group of interest
+  else {
+    
+  if(relevel == "Diptera"){
+    combined <- combined %>% mutate(Order = relevel(Order, ref = "Diptera"))
+  }
+  if(relevel == "Coleoptera"){
+    combined <- combined %>% mutate(Order = relevel(Order, ref = "Coleoptera"))
+  }
+  if(relevel == "Hymenoptera"){
+    combined <- combined %>% mutate(Order = relevel(Order, ref = "Hymenoptera"))
+  }
+  if(relevel == "Hemiptera"){
+    combined <- combined %>% mutate(Order = relevel(Order, ref = "Hemiptera"))
+  }
+  if(relevel == "Lepidoptera"){
+    combined <- combined %>% mutate(Order = relevel(Order, ref = "Lepidoptera"))
+  }}
+  
+  
+  group_colours <- c(Diptera = "#FFC300", Lepidoptera = "#ff5733", Hymenoptera = "#c70039", Hemiptera = "#900c3f", Coleoptera = "#581845")
+  
+  plot <- combined %>%
+    ggplot()+
+    aes(x = Predominant_land_use, y = Percent_diff, colour = Order, group = Order)+
+    geom_hline(yintercept = 0, linewidth = 0.5, color = ("black"), linetype = 1)+
+    geom_point(size = 3, position = position_dodge(width = 0.5))+
+    geom_linerange(aes(ymin = Percent_lower, ymax = Percent_upper), position = position_dodge(width = 0.5), size = 1)+
+    theme_classic()+
+    scale_x_discrete(limits=comb_plot_limits)+
+    geom_vline(xintercept = 1.5, linetype = 2)+
+    geom_vline(xintercept = 2.5, linetype = 2)+
+    geom_vline(xintercept = 3.5, linetype = 2)+
+    geom_vline(xintercept = 4.5, linetype = 2)+
+    geom_vline(xintercept = 5.5, linetype = 2)+
+    geom_vline(xintercept = 6.5, linetype = 2)+
+    geom_vline(xintercept = 7.5, linetype = 2)+
+    theme(axis.text.x = element_text(face= "bold", angle = 45, hjust = 1),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(face = "bold"),
+          panel.border = element_rect(colour = "black",  fill=NA))+
+    xlab("Land use intensity class")+
+    ylab("Total abundance difference (%)") +
+    scale_colour_manual(values=group_colours) + # this overrides the colours for the groups as above
+    guides(color = guide_legend(
+      override.aes=list(shape = 15, size = 8)))
+
+  return(plot)
+}
---
+++ scripts/predict_this_model.R
@@ -0,0 +1,72 @@
+# Function for predicting abundance or species richness percentage differences
+# From baseline of primary veg based on model predictions
+
+predict_this_model <- function(model, order) {
+  
+  # gather the effects and confidence intervals using simulation. Simulate 1000 times
+  effects <- FEsim(model, n.sims = 1000)
+  
+  # make the term column a factor so it can be recoded
+  effects$term <- as.factor(effects$term)
+  
+  # pull out effects
+  effects <- effects %>%
+    slice(1:8)
+  
+  # rename factors for ease of visualisation
+  effects$term <- recode_factor(effects$term, "(Intercept)" = "Primary",
+                                "Predominant_land_useYoung secondary vegetation" = "YSV",
+                                "Predominant_land_useIntermediate secondary vegetation" = "ISV",
+                                "Predominant_land_useMature secondary vegetation" = "MSV",
+                                "Predominant_land_usePlantation forest" = "Plantation",
+                                "Predominant_land_usePasture" = "Pasture",
+                                "Predominant_land_useCropland" = "Cropland",
+                                "Predominant_land_useUrban" = "Urban")
+  
+  
+  # Add in upper and lower confidence intervals 
+  effects <- effects %>%
+    mutate(Upper_ci = (median + 1.96*sd)) %>%
+    mutate(Lower_ci = (median - 1.96*sd))
+  
+  # Back transform the log abundance estimates so that I get a measure of abundance
+  effects <- effects %>%
+    mutate(
+      Percent_diff = (((exp(median[1] + median) - 1) / (exp(median[1]) - 1))*100)-100) %>%
+    mutate(
+      Percent_upper = (((exp(median[1] + Upper_ci) - 1) / (exp(median[1]) - 1)) * 100) - 100) %>%
+    mutate(
+      Percent_lower = (((exp(median[1] + Lower_ci) - 1) / (exp(median[1]) - 1)) * 100) - 100)
+  
+  
+  # Shift the baseline down to 0
+  effects[1,7] <- 0 #shift median
+  effects[1,8] <- 0 #shift upper
+  effects[1,9] <- 0 #shift lower 
+  
+  # Rename the sim eff term column name for joining
+  colnames(effects)[1] <- "Predominant_land_use"
+  
+  # Add in a column of Order to make sure there is a grouping variable
+  if(order == "Diptera"){
+   effects <- effects %>% mutate(Order = "Diptera")
+  }
+  if(order == "Coleoptera"){
+    effects <- effects %>% mutate(Order = "Coleoptera")
+  }
+  if(order == "Hymenoptera"){
+    effects <- effects %>% mutate(Order = "Hymenoptera")
+  }
+  if(order == "Hemiptera"){
+    effects <- effects %>% mutate(Order = "Hemiptera")
+  }
+  if(order == "Lepidoptera"){
+    effects <- effects %>% mutate(Order = "Lepidoptera")
+  }
+  
+  # make the term column a factor so it can be recoded
+  effects$Order <- as.factor(effects$Order)
+  
+  return(effects)
+}
+
